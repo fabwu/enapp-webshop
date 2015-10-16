1786a37011d35c5bec97844b0472b4ad0ca17ce9
Fabian Wüthrich <fabian.wuethrich.01@stud.hslu.ch>
2015-10-16 09:46:09 +0200

    Add project

---
+++ WebshopEJB/ejbModule/deleteMe/DeleteMe.java
@@ -0,0 +1,20 @@
+package deleteMe;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class DeleteMe
+ */
+@Stateless
+@LocalBean
+public class DeleteMe {
+
+    /**
+     * Default constructor. 
+     */
+    public DeleteMe() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
